2e1e15383c14d81ff10f0acaeaae29a0b78e5abf
Priyanka Kishore <33528287+priyanka-kishore@users.noreply.github.com>
2026-01-02 01:24:00 -0500

    Time: 0 ms (100%), Space: 17.4 MB (80.55%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -9,33 +9,14 @@ class Solution:
         lo = 0
         hi = len(nums) - 1
 
-        if len(nums) == 1:
-            return
-
         i = 0
         while i <= hi:
-            # print(nums, nums[i])
             if nums[i] == 1:
                 i += 1
-
             elif nums[i] == 2: # high number
-                # swap
-                tmp = nums[i]
-                nums[i] = nums[hi]
-                nums[hi] = tmp
-
+                nums[i], nums[hi] = nums[hi], nums[i] # swap
                 hi -= 1
-                # print(f'HIGH number... swapping and decrementing hi to index {hi}')
-                continue
-            
             elif nums[i] == 0: # low number
-                # swap
-                tmp = nums[i]
-                nums[i] = nums[lo]
-                nums[lo] = tmp
+                nums[i], nums[lo] = nums[lo], nums[i] # swap
                 lo += 1
                 i += 1
-                # print(f'LOW number... swapping and incrementing lo to index {lo}')
-                continue
-            
-            

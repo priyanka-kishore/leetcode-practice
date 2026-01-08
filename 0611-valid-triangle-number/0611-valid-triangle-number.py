183b3767732b0d2a51281b61f1b001ecb39625d7
Priyanka Kishore <33528287+priyanka-kishore@users.noreply.github.com>
2026-01-07 22:10:39 -0500

    Time: 439 ms (40.11%), Space: 19.5 MB (8.98%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.py
+++ 0611-valid-triangle-number/0611-valid-triangle-number.py
@@ -1,24 +1,9 @@
 class Solution:
     def triangleNumber(self, nums: List[int]) -> int:
-        nums.sort()
         count = 0
+        nums.sort()
 
-        # Brute force - O(n^3) - but i correctly figured out the key! only need to satisfy a + b > c
-        # for a in range(len(nums) - 2):
-        #     for b in range(a+1, len(nums) - 1):
-        #         for c in range(b + 1, len(nums)):
-        #             if nums[c] >= nums[a] + nums[b]: # if largest side is too big, rest of array is too big so skip
-        #                 break
-        #             count += 1
-
-
-        # Efficient - i had same idea (basically opposite logic of brute force key), 
-        # start c from end, and do 2-pointer with a + b on opposite ends before c
-        # based on a+b>c requirement, the largest b that satisfies it will guarantee that all the previous a's will satisfy it bc they only get bigger
-        # if curr b does not satisfy a+b>c requirement (meaning a+b is too small), increment a (to increase a+b)
         for c in range(len(nums) - 1, 1, -1):
-
-            # two-pointer:
             a = 0
             b = c - 1
 
@@ -28,6 +13,5 @@ class Solution:
                     b -= 1
                 else:
                     a += 1
-
 
         return count

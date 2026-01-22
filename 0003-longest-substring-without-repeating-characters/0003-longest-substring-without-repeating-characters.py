8d3362981e6c85ac67a139c24e5ecb6fc888e3c2
Priyanka Kishore <33528287+priyanka-kishore@users.noreply.github.com>
2026-01-21 20:27:39 -0500

    Time: 11 ms (90.29%), Space: 19.2 MB (26.04%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -7,21 +7,12 @@ class Solution:
         start = 0
         max_len = 0
 
-        for i in range(len(s)):
-
-            if s[i] not in seen:
-                seen[s[i]] = i # add to set
-                max_len = max(max_len, i - start + 1) # check max_len
-            else:
-                # re-adjust window: move start to index AFTER first seen occurrence
-                start = seen[s[i]] + 1 # NO: MOVING WINDOW BACKWARDS IS DANGEROUD
-
-                # don't forget to REMOVE dict elements where value < index of first seen occurrence
-                seen = {char: index for char, index in seen.items() if index >= seen[s[i]]} # NO: REBUILDING THE DICT IS O(N^2) IN WORST CASE
-                # and update index of repeated occurrence
-                seen[s[i]] = i
-
-                # don't forget to recalc that new length after moving start forward
-                max_len = max(max_len, i - start + 1)
+        for i, char in enumerate(s):
+            if char in seen:
+                # shrink window start to after the first seen occurrence or `start` (whichever is further right, bc should not move window start backwards!)
+                start = max(start, seen[char] + 1)
+            
+            seen[char] = i # update last seen position
+            max_len = max(max_len, i - start + 1) # check length of current window
         
         return max_len

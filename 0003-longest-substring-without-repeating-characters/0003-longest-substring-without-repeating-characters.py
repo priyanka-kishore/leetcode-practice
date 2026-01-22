78cd8f99269ac4c17c0e6ef0baef5685f72e5164
Priyanka Kishore <33528287+priyanka-kishore@users.noreply.github.com>
2026-01-21 20:03:43 -0500

    Time: 211 ms (9.56%), Space: 19.4 MB (19.71%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -14,10 +14,10 @@ class Solution:
                 max_len = max(max_len, i - start + 1) # check max_len
             else:
                 # re-adjust window: move start to index AFTER first seen occurrence
-                start = seen[s[i]] + 1
+                start = seen[s[i]] + 1 # NO: MOVING WINDOW BACKWARDS IS DANGEROUD
 
                 # don't forget to REMOVE dict elements where value < index of first seen occurrence
-                seen = {char: index for char, index in seen.items() if index >= seen[s[i]]}
+                seen = {char: index for char, index in seen.items() if index >= seen[s[i]]} # NO: REBUILDING THE DICT IS O(N^2) IN WORST CASE
                 # and update index of repeated occurrence
                 seen[s[i]] = i
 

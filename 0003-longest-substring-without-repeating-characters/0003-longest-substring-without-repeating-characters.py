d6d3ceeba8b879252a2d4029a17ad2024d489aa0
Priyanka Kishore <33528287+priyanka-kishore@users.noreply.github.com>
2026-01-21 19:59:16 -0500

    Time: 207 ms (9.71%), Space: 19.3 MB (19.71%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,19 +1,27 @@
 class Solution:
-    # O(n) time, O(n) space
     def lengthOfLongestSubstring(self, s: str) -> int:
-        L = R = max_len = 0
-        seen = set()
+        if len(s) <= 1:
+            return len(s)
 
-        while R < len(s):
-            # if R char is not dupe, add to set, move R++, increase max
-            if s[R] not in seen:
-                seen.add(s[R])
-                curr_len = R - L + 1
-                max_len = max(curr_len, max_len)
-                R += 1
+        seen = {} # char-index pairs {'a': 0 }
+        start = 0
+        max_len = 0
+
+        for i in range(len(s)):
+
+            if s[i] not in seen:
+                seen[s[i]] = i # add to set
+                max_len = max(max_len, i - start + 1) # check max_len
             else:
-                # move Left pointer up one
-                seen.remove(s[L])
-                L += 1
+                # re-adjust window: move start to index AFTER first seen occurrence
+                start = seen[s[i]] + 1
 
+                # don't forget to REMOVE dict elements where value < index of first seen occurrence
+                seen = {char: index for char, index in seen.items() if index >= seen[s[i]]}
+                # and update index of repeated occurrence
+                seen[s[i]] = i
+
+                # don't forget to recalc that new length after moving start forward
+                max_len = max(max_len, i - start + 1)
+        
         return max_len
